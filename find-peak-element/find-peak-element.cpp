6b92f6f62776f0fa20795770f6dc7674fc30a50d
Kailash Raj Gaur <kailashgaur10@gmail.com>
2022-03-19 02:37:36 +0530

    Time: 4 ms, Memory: 8.9 MB - LeetHub

--- find-peak-element/find-peak-element.cpp
+++ find-peak-element/find-peak-element.cpp
@@ -11,7 +11,7 @@ public:
         int low = 0, high = len;
         while(low<high){
             int mid = low+(high-low)/2;
-            if(mid!=0 && nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1])
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1])
                 return mid;
             else if(nums[mid-1] < nums[mid])
                 low = mid+1;
