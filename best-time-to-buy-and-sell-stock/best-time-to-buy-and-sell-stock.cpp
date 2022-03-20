85971bef1915305599562b26eef44dfb5123a7b4
Kailash Raj Gaur <kailashgaur10@gmail.com>
2022-03-20 19:40:08 +0530

    Time: 207 ms, Memory: 93.3 MB - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,17 +4,17 @@ public:
         if(prices.size()==1) return 0;
         
         int maxi = 0;
-        int i=0, j=1;
-        
-        while( j < prices.size() ){
-            
-            if( prices[j]-prices[i] > maxi ) maxi=prices[j]-prices[i];
-            
-            else if (prices[j] < prices[i]) i=j;
-            
-            j++;
-            
-        }
+//         int i=0, j=1;
+//         while( j < prices.size() ){
+//             if( prices[j]-prices[i] > maxi ) maxi=prices[j]-prices[i];
+//             else if (prices[j] < prices[i]) i=j;
+//             j++;
+//         }
+        int mini = prices[0];
+        for(int i=1; i<prices.size(); i++){
+            maxi = max(maxi, prices[i]-mini);
+            mini = min(mini, prices[i]);
+        }        
         return maxi;
     }
 };
